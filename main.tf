63b24a4c2c1bdd03e22ca9b159f1d982b75606de
saravananmani1988 <saravanan.mani1988@gmail.com>
2021-12-21 20:44:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,32 +9,32 @@ terraform {
   }
 }
 
-provider "intersight" {
-  apikey    = var.apikey
-  secretkey = var.secretkey
-  endpoint  = var.endpoint
-}
+# provider "intersight" {
+#   apikey    = var.apikey
+#   secretkey = var.secretkey
+#   endpoint  = var.endpoint
+# }
   
-data "intersight_organization_organization" "organization_moid" {
-name = "default"
-}
+# data "intersight_organization_organization" "organization_moid" {
+# name = "default"
+# }
 
-output "PodX_organization_moid" {
-description = "organization id "
-value = data.intersight_organization_organization.organization_moid.id
-}
+# output "PodX_organization_moid" {
+# description = "organization id "
+# value = data.intersight_organization_organization.organization_moid.id
+# }
 
-  resource "intersight_ntp_policy" "ntp1" {
-  name        = "ntp1-tfc"
-  description = "test policy"
-  enabled     = true
-  ntp_servers = [
-    "ntp.esl.cisco.com",
-    "time-a-g.nist.gov",
-    "time-b-g.nist.gov"
-  ]
-  organization {
-    object_type = "organization.Organization"
-    moid        = data.intersight_organization_organization.organization_moid.id
-  }
-}
+#   resource "intersight_ntp_policy" "ntp1" {
+#   name        = "ntp1-tfc"
+#   description = "test policy"
+#   enabled     = true
+#   ntp_servers = [
+#     "ntp.esl.cisco.com",
+#     "time-a-g.nist.gov",
+#     "time-b-g.nist.gov"
+#   ]
+#   organization {
+#     object_type = "organization.Organization"
+#     moid        = data.intersight_organization_organization.organization_moid.id
+#   }
+# }
