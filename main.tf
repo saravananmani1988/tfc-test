3f12138091ad8f5b1de9c6a4aa4b93de54252401
saravananmani1988 <saravanan.mani1988@gmail.com>
2021-12-21 20:53:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,26 +15,138 @@ provider "intersight" {
   endpoint  = var.endpoint
 }
   
-data "intersight_organization_organization" "organization_moid" {
-name = "default"
-}
+module "terraform-intersight-iks" {
 
-output "PodX_organization_moid" {
-description = "organization id "
-value = data.intersight_organization_organization.organization_moid.id
-}
+  source  = "terraform-cisco-modules/iks/intersight//"
+  version = "2.1.0"
 
-  resource "intersight_ntp_policy" "ntp1" {
-  name        = "ntp1-tfc"
-  description = "test policy"
-  enabled     = true
-  ntp_servers = [
-    "ntp.esl.cisco.com",
-    "time-a-g.nist.gov",
-    "time-b-g.nist.gov"
-  ]
-  organization {
-    object_type = "organization.Organization"
-    moid        = data.intersight_organization_organization.organization_moid.id
+# Kubernetes Cluster Profile  Adjust the values as needed.
+  cluster = {
+    name                = "new_cluster_ist"
+    action              = "Unassign"
+    wait_for_completion = false
+    worker_nodes        = 1
+    load_balancers      = 1
+    worker_max          = 2
+    control_nodes       = 1
+    ssh_user            = var.ssh_user
+    ssh_public_key      = var.ssh_key
   }
+
+
+# IP Pool Information (To create new change "use_existing" to 'false' uncomment variables and modify them to meet your needs.)
+  ip_pool = {
+    use_existing        = false
+    name                = "New-IP-Pool-IST"
+     ip_starting_address = "10.0.208.165"
+     ip_pool_size        = "05"
+     ip_netmask          = "255.255.255.0"
+     ip_gateway          = "10.0.208.1"
+     dns_servers         = ["10.0.208.135"]
+  }
+
+# Sysconfig Policy (UI Reference NODE OS Configuration) (To create new change "use_existing" to 'false' uncomment variables and modify them to meet your needs.)
+  sysconfig = {
+    use_existing = true
+    name         = "stovl-k8s-sys-config-policy"
+    # domain_name  = "rich.ciscolabs.com"
+    # timezone     = "America/New_York"
+    # ntp_servers  = ["10.101.128.15"]
+    # dns_servers  = ["10.101.128.15"]
+  }
+
+# Kubernetes Network CIDR (To create new change "use_existing" to 'false' uncomment variables and modify them to meet your needs.)
+  k8s_network = {
+    use_existing = true
+    name         = "stovl-k8s-network-policy"
+
+    ######### Below are the default settings.  Change if needed. #########
+    # pod_cidr     = "100.65.0.0/16"
+    # service_cidr = "100.64.0.0/24"
+    # cni          = "Calico"
+  }
+# Version policy (To create new change "useExisting" to 'false' uncomment variables and modify them to meet your needs.)
+  versionPolicy = {
+    useExisting = true
+    policyName     = "version-1-19-15-ssm"
+    iksVersionName = "1.19.15-iks-1"
+  }
+# Trusted Registry Policy (To create new change "use_existing" to 'false' and set "create_new' to 'true' uncomment variables and modify them to meet your needs.)
+# Set both variables to 'false' if this policy is not needed.
+  tr_policy = {
+    use_existing = false
+    create_new   = false
+    name         = "trusted-registry"
+  }
+# Runtime Policy (To create new change "use_existing" to 'false' and set "create_new' to 'true' uncomment variables and modify them to meet your needs.)
+# Set both variables to 'false' if this policy is not needed.
+  runtime_policy = {
+    use_existing = false
+    create_new   = false
+    # name                 = "runtime"
+    # http_proxy_hostname  = "t"
+    # http_proxy_port      = 80
+    # http_proxy_protocol  = "http"
+    # http_proxy_username  = null
+    # http_proxy_password  = null
+    # https_proxy_hostname = "t"
+    # https_proxy_port     = 8080
+    # https_proxy_protocol = "https"
+    # https_proxy_username = null
+    # https_proxy_password = null
+  }
+
+# Infrastructure Configuration Policy (To create new change "use_existing" to 'false' and uncomment variables and modify them to meet your needs.)
+  infraConfigPolicy = {
+    use_existing = true
+    # platformType = "iwe"
+    # targetName   = "falcon"
+    policyName   = "stovl-vcenter-infra"
+    # description  = "Test Policy"
+    # interfaces   = ["iwe-guests"]
+    # vcTargetName   = optional(string)
+    # vcClusterName      = optional(string)
+    # vcDatastoreName     = optional(string)
+    # vcResourcePoolName = optional(string)
+    # vcPassword      = optional(string)
+  }
+
+# Addon Profile and Policies (To create new change "createNew" to 'true' and uncomment variables and modify them to meet your needs.)
+# This is an Optional item.  Comment or remove to not use.  Multiple addons can be configured.
+  addons       = {
+    createNew = false
+    use_existing = false
+    # {
+    # addonPolicyName = "smm-tf"
+    # addonName            = "smm"
+    # description       = "SMM Policy"
+    # upgradeStrategy  = "AlwaysReinstall"
+    # installStrategy  = "InstallOnly"
+    # releaseVersion = "1.7.4-cisco4-helm3"
+    # overrides = yamlencode({"demoApplication":{"enabled":true}})
+    # }
+    
+    createNew = false
+    use_existing = false
+    # {
+    # addonName            = "ccp-monitor"
+    # description       = "monitor Policy"
+    # upgradeStrategy  = "AlwaysReinstall"
+    # installStrategy  = "InstallOnly"
+    # releaseVersion = "0.2.61-helm3"
+    # overrides = yamlencode({"demoApplication":{"enabled":true}})
+    # }
+    }
+# Worker Node Instance Type (To create new change "use_existing" to 'false' and uncomment variables and modify them to meet your needs.)
+  instance_type = {
+    use_existing = true
+    name         = "stovl-node-profile"
+    # cpu          = 4
+    # memory       = 16386
+    # disk_size    = 40
+  }
+
+# Organization and Tag Information
+  organization = "default"
+  tags         = "IST"
 }
