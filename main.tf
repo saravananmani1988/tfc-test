15e54daaa99dd6aa25ed501b693c632363aedd21
saravananmani1988 <saravanan.mani1988@gmail.com>
2021-12-21 20:48:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,17 +24,17 @@ description = "organization id "
 value = data.intersight_organization_organization.organization_moid.id
 }
 
-#   resource "intersight_ntp_policy" "ntp1" {
-#   name        = "ntp1-tfc"
-#   description = "test policy"
-#   enabled     = true
-#   ntp_servers = [
-#     "ntp.esl.cisco.com",
-#     "time-a-g.nist.gov",
-#     "time-b-g.nist.gov"
-#   ]
-#   organization {
-#     object_type = "organization.Organization"
-#     moid        = data.intersight_organization_organization.organization_moid.id
-#   }
-# }
+  resource "intersight_ntp_policy" "ntp1" {
+  name        = "ntp1-tfc"
+  description = "test policy"
+  enabled     = true
+  ntp_servers = [
+    "ntp.esl.cisco.com",
+    "time-a-g.nist.gov",
+    "time-b-g.nist.gov"
+  ]
+  organization {
+    object_type = "organization.Organization"
+    moid        = data.intersight_organization_organization.organization_moid.id
+  }
+}
