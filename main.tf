558faded0c9f6a3acf661f489a1ac02bf05796c2
saravananmani1988 <saravanan.mani1988@gmail.com>
2021-12-21 20:26:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,24 @@
+terraform {
+  required_version = ">=0.14.5"
 
+  required_providers {
+    intersight = {
+      source  = "CiscoDevNet/intersight"
+      version = ">=1.0.18"
+    }
+  }
+
+provider "intersight" {
+  apikey    = var.apikey
+  secretkey = var.secretkey
+  endpoint  = var.endpoint
+}
+  
+data "intersight_organization_organization" "organization_moid" {
+name = "default"
+}
+
+output "PodX_organization_moid" {
+description = "organization id "
+value = data.intersight_organization_organization.organization_moid.id
+}
