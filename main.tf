017659030c8d3812dce16b3f4004063904452791
saravananmani1988 <saravanan.mani1988@gmail.com>
2022-01-11 14:36:54 +0530

    Update main.tf
    deploy cluster

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "terraform-intersight-iks" {
 # Kubernetes Cluster Profile  Adjust the values as needed.
   cluster = {
     name                = "new_cluster_ist"
-    action              = "Unassign"
+    action              = "Deploy"
     wait_for_completion = false
     worker_nodes        = 1
     load_balancers      = 1
